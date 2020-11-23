db0d51326c7fb46d85306987227e3bc8ca657c19
BearWulF <elias.sidi@gmail.com>
2020-11-23 18:23:21 -0500

    Update ProductTest.java

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -21,7 +21,7 @@ public class ProductTest {
         // I am making this test to fail
         double a = 180;
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the price of the product", Double.parseDouble("180"), aProduct.getPrice());
+        assertEquals("Check the price of the product", 180, aProduct.getPrice(),0.001);
     }
 
 
