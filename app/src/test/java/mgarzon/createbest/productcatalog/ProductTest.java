144c4638d47dfd3d129de2a35e9147c26e528c37
BearWulF <elias.sidi@gmail.com>
2020-11-23 18:11:48 -0500

    Update ProductTest.java

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -19,7 +19,7 @@ public class ProductTest {
     @Test
     public void checkProductPrice() {
         // I am making this test to fail
-        double a = 180.0;
+        double a = 180;
         Product aProduct = new Product("1", "DELL MONITOR", 180);
         assertEquals("Check the price of the product", a, aProduct.getPrice());
     }
